932600293274ef1babc1a96a5a542090c68e5f98
Kosma Dunikowski <kosmadunikowski@gmail.com>
2012-07-03 12:20:48 +0200

    test for getting results from a query

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -7,4 +7,13 @@ describe "home page", :js => true do
     page.should have_content('Dict')
   end
 
+  it "shows some results after max. 5 seconds from submiting a query" do
+    visit '/'
+    fill_in 'query_field', :with => 'sample query'
+    click_link 'Search'
+
+    sleep 5 # not really a good idea, but can't think of anything better at a time
+    page.has_xpath?("//div[@id='results_area']/*").should eq true
+  end
+
 end
