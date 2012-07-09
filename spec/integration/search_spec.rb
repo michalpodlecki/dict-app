f9020bf5e395a67eab5ea9785a21dcd5108d4536
Jan Borwin <jan.borwin@gmail.com>
2012-07-10 00:27:44 +0200

    [#32352495] test for getting result when pressing logo

--- spec/integration/search_spec.rb
+++ spec/integration/search_spec.rb
@@ -15,4 +15,14 @@ describe "search page", :js => true do
     page.has_xpath?("//div[@id='results_area']/*").should eq true
   end
 
+  it "should take to the home page" do
+    visit '/'
+    old_url = page.evaluate_script("document.URL")
+    visit '/smok'
+    click_link('search_button')
+    after_search = click_link('home_link')
+    new_url = page.evaluate_script("document.URL")
+    (old_url == new_url).should eq true
+  end
+
 end
