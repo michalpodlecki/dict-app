06c0c7ac6e89044e90ad359c8821b818dc5da990
Kosma Dunikowski <kosmadunikowski@gmail.com>
2012-07-09 13:28:05 +0200

    [#32202669] test for results claring upon each new search query

--- spec/integration/ui_spec.rb
+++ spec/integration/ui_spec.rb
@@ -8,4 +8,16 @@ describe "ui", :js => true do
   #   page.execute_script("window.render_results(\"test\");");
   # end
 
+  it "clears the previous results when new search is issued" do
+    visit '/'
+    page.execute_script("window.render_results(\"test\");");
+    before_search = page.find_by_id('results_area').native
+
+    fill_in('query_field', :with => 'smok')
+    click_link('search_button')
+    after_search = page.find_by_id('results_area').native
+
+    (before_search == after_search).should eq true
+  end
+
 end
