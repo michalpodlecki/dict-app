d1b47700f03d1f6de7fd2755a948f661fb454221
Kosma Dunikowski <kosmadunikowski@gmail.com>
2012-07-10 12:30:55 +0200

    added test for Search#get_results_for

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe Search do
-  context "Search#get_single_service" do
+  describe "#get_single_service" do
     it "should raise LoadError when Dict thrown exception" do
       search = Search.new
       Dict.stub(:get_single_dictionary_translations).and_raise()
       lambda { search.get_single_service("", "") }.should raise_exception(Search::LoadError)
+    end
+  end
+
+  describe "#get_results_for" do
+    it "returns results for each available service" do
+      search = Search.new
+      search.stub(:get_single_service).and_return("")
+
+      services = %w{service1, service2}
+      search.get_results_for(services, "sample_query").length.should eq services.length
     end
   end
 end
