6f3b917a1cdd4a1c01d83576b072c04ae6e615c8
Kosma Dunikowski <kosmadunikowski@gmail.com>
2012-07-19 14:26:32 +0200

    [#33036499] additional media queries for mobile devices

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ DictApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.precompile += ['mobile.css']
   config.assets.compile = false
 
   # Generate digests for assets URLs
